faf6bec3cc5a847e5801b5d9303f698b25f04447
jleoscorpion-creator <jleo.scorpion@gmail.com>
2026-05-11 23:01:46 -0600

    Actualizar gemini.ts

--- netlify/functions/gemini.ts
+++ netlify/functions/gemini.ts
@@ -18,7 +18,7 @@ export const handler: Handler = async (event, context) => {
     const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-1.5-flash",
+  model: "gemini-2.0-flash",
 });
 
     // Modo chat: interactuar con un mensaje del usuario
